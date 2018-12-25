c9ac468d8ba5af59178b770fbeaea1c934900925
FRAMGIA\tran.van.cu <tran.van.cu@framgia.com>
2018-12-25 13:27:21 +0700

    update

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,11 +3,10 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
-@RequestMapping("/")
+@RequestMapping(value = "/")
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping(value = "/hello")
     public String helloWorld() {
         return "hello world";
     }
