2b51d2367b2f56524283f2b1e8c1303d115efac3
FRAMGIA\tran.van.cu <tran.van.cu@framgia.com>
2019-02-20 17:00:35 +0700

    test log

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 import com.example.demo.model.Employee;
-import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
